b614f23f3b8f17308b7c53d8b161c1125ff75ede
Nail Husainov <1nprogh1@gmail.com>
2024-03-27 21:58:07 +0300

    First part of lab03

---
+++ Lab03Client/Lab03Client/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+try
+{
+    await client.ConnectAsync("127.0.0.1", 8888);
+    var responseBytes = new byte[512];
+    int bytes;
+    Console.WriteLine($"Подключение к {client.RemoteEndPoint} установлено");
+    string response = "";
+    string items = "";
+    string temp;
+    string msg;
+    string zapros = "";
+    Console.WriteLine("Enter action (1-put in a file,2 - get a file,3 delete a file,exit-for exit):>");
+    temp = Console.ReadLine();
+    while (true)
+    {
+        if (temp == "1" || temp == "2" || temp == "3")
+        {
+            zapros += temp + "`";
+            break;
+        }
+        else if (temp == "exit")
+        {
+            client.Send(Encoding.UTF8.GetBytes(temp));
+            bytes = await client.ReceiveAsync(responseBytes);
+            response = Encoding.UTF8.GetString(responseBytes, 0, bytes);
+            if (response == "1") client.Close(); break;
+        }
+        else
+        {
+            Console.WriteLine("Введите корректное значение! \n Enter action (1-put in a file,2 - get a file,3 delete a file,exit-for exit):>");
+            temp = Console.ReadLine();
+        }
+    }
+    Console.WriteLine("Enter filename:>");
+    zapros += Console.ReadLine() + "`";
+    if (temp == "1")
+    {
+        Console.WriteLine("Enter  file content: >");
+        zapros += Console.ReadLine();
+        Console.WriteLine("The request was sent.");
+        client.Send(Encoding.UTF8.GetBytes(zapros));
+        bytes = await client.ReceiveAsync(responseBytes);
+        response = Encoding.UTF8.GetString(responseBytes, 0, bytes);
+        if (response == "202")
+        {
+            Console.WriteLine("The response says that the file was created!");
+        }
+        else
+        {
+            Console.WriteLine("The response says that creating the file was forbidden!");
+        }
+    }
+    else if (temp == "2")
+    {
+        Console.WriteLine("The request was sent.");
+        client.Send(Encoding.UTF8.GetBytes(zapros));
+        bytes = await client.ReceiveAsync(responseBytes);
+        response = Encoding.UTF8.GetString(responseBytes, 0, bytes);
+        var temp1 = response.Split("`");
+        if (response[0].ToString() == "2")
+        {
+            Console.WriteLine("The content of the file is: " + temp1[1]);
+        }
+        else
+        {
+            Console.WriteLine("The response says that the file was not found!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("The request was sent.");
+        client.Send(Encoding.UTF8.GetBytes(zapros));
+        bytes = await client.ReceiveAsync(responseBytes);
+        response = Encoding.UTF8.GetString(responseBytes, 0, bytes);
+        if (response == "200")
+        {
+            Console.WriteLine("The response says that the file was successfully deleted!");
+        }
+        else
+        {
+            Console.WriteLine("The response says that the file was not found!");
+        }
+    }
+    client.Close();
+}
+catch
+{
+    Console.WriteLine($"Не удалось установить подключение");
+}
+finally
+{
+    client.Close();
+}
---
+++ Lab03Server/Lab03Server/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 8888);
+using Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+server.Bind(ipPoint);
+server.Listen();
+Console.WriteLine($"Server started!");
+try
+{
+    while (true) // Цикл для ожидания новых подключений
+    {
+        using Socket client = await server.AcceptAsync();
+        Console.WriteLine($"Адрес подключенного клиента: {client.RemoteEndPoint}");
+        byte[] b = new byte[512];
+        var files = new List<(int, string)>();
+        try
+        {
+            string temp = "";
+            client.Receive(b);
+            string msg = Encoding.Default.GetString(b);
+            string[] zpr = new string[3];
+            zpr = msg.Split("`");
+            if (zpr[0].Contains("exit"))
+            {
+                client.Send(Encoding.UTF8.GetBytes("1"));
+                break;
+            }
+            else if (zpr[0] == "1")
+            {
+                var res = PUT(zpr[1], zpr[2]);
+                await client.SendAsync(Encoding.UTF8.GetBytes(res));
+            }
+            else if (zpr[0] == "2")
+            {
+                var res = GET(zpr[1]);
+                await client.SendAsync(Encoding.UTF8.GetBytes(res));
+            }
+            else
+            {
+                var res = DELETE(zpr[1]);
+                await client.SendAsync(Encoding.UTF8.GetBytes(res));
+            }
+            client.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Клиент отключился ");
+        }
+        finally
+        {
+
+            client.Close();
+        }
+    }
+}
+finally
+{
+    server.Close();
+}
+
+
+string PUT(string fileName,string text)
+{
+    if (File.Exists("Server\\data\\" + fileName))
+    {
+        return "403";
+    }
+    else
+    {
+        using(var stream= File.Open("Server\\data\\" + fileName, FileMode.CreateNew,FileAccess.Write))
+        {
+            stream.Write(Encoding.UTF8.GetBytes(text));
+        }
+        return "202";
+    }
+}
+
+string GET(string fileName)
+{
+    if (!File.Exists("Server\\data\\" + fileName))
+    {
+        return "404";
+    }
+    else
+    {
+        string temp = File.ReadAllText("Server\\data\\" + fileName);
+        return "200`"+temp;
+    }
+}
+string DELETE(string fileName)
+{
+    if (!File.Exists("Server\\data\\" + fileName))
+    {
+        return "404";
+    }
+    else
+    {
+        File.Delete("Server\\data\\" + fileName);
+        return "200";
+    }
+}
