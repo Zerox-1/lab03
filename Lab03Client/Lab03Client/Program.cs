c44c20f85fa6301cba3bbf05b0e2b22aa180b1e0
Nail Husainov <1nprogh1@gmail.com>
2024-03-29 04:45:38 +0300

    Second part of lab03

--- Lab03Client/Lab03Client/Program.cs
+++ Lab03Client/Lab03Client/Program.cs
@@ -1,91 +1,146 @@
 ﻿using System;
 using System.Net.Sockets;
 using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 try
 {
     await client.ConnectAsync("127.0.0.1", 8888);
-    var responseBytes = new byte[512];
-    int bytes;
+    var responseBytes = new byte[8192];
+    int bytes=0;
     Console.WriteLine($"Подключение к {client.RemoteEndPoint} установлено");
     string response = "";
-    string items = "";
+    string temp2 = "";
     string temp;
-    string msg;
-    string zapros = "";
-    Console.WriteLine("Enter action (1-put in a file,2 - get a file,3 delete a file,exit-for exit):>");
-    temp = Console.ReadLine();
     while (true)
     {
-        if (temp == "1" || temp == "2" || temp == "3")
+        temp = getOperation();
+        if (temp == "exit")
         {
-            zapros += temp + "`";
+            client.Close();
             break;
         }
-        else if (temp == "exit")
+        else if (temp == "2")
         {
-            client.Send(Encoding.UTF8.GetBytes(temp));
+            Console.WriteLine("Do you want to get the file by name or by id (1 - name, 2 - id): >");
+            temp2 = Console.ReadLine();
+            if (temp2 == "1")
+            {
+                Console.WriteLine("Enter name: >");
+                temp2 = Console.ReadLine();
+                if (temp2.Contains("txt"))
+                {
+                    client.Send(Encoding.UTF8.GetBytes("2`1`" + temp2 + "`"));
+                }
+                else
+                {
+                    client.Send(Encoding.UTF8.GetBytes("2`3`" + temp2 + "`"));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter ID: >");
+                 string temp3 = Console.ReadLine();
+                if (temp2.Contains("1"))
+                {
+                    client.Send(Encoding.UTF8.GetBytes("2`2`" + temp3 + "`"));
+                }
+                else
+                {
+                    client.Send(Encoding.UTF8.GetBytes("2`4`" + temp3 + "`"));
+                }
+            }
             bytes = await client.ReceiveAsync(responseBytes);
             response = Encoding.UTF8.GetString(responseBytes, 0, bytes);
-            if (response == "1") client.Close(); break;
+            var items = response.Split("`");
+            if (items[0] == "200")
+            {
+                if (temp2.Contains("txt"))
+                {
+                    using (StreamWriter writer = new StreamWriter("client\\data\\" + "temp.txt", false))
+                    {
+                        writer.Write(items[1]);
+                    }
+                    Console.WriteLine("The File was downloaded! Specify a name for it: >");
+                    string tempQ = Console.ReadLine();
+                    while (true)
+                    {
+                        if (tempQ == "temp.txt")
+                        {
+                            Console.WriteLine("Wrong name!");
+                            tempQ = Console.ReadLine();
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    File.Move("client\\data\\" + "temp.txt", "client\\data\\" + tempQ);
+                    File.Create("client\\data\\" + "temp.txt");
+                }
+                else
+                {
+                    byte[] imageBytes = new byte[Convert.ToInt32(items[1])];
+                    bytes = await client.ReceiveAsync(imageBytes);
+                    Console.WriteLine("The File was downloaded! Specify a name for it: >");
+                    string tempQ = Console.ReadLine();
+                    using (var ms12 = new MemoryStream(imageBytes))
+                    {
+                        Image img = Image.FromStream(ms12);
+                        img.Save("client\\data\\" + tempQ, img.RawFormat);
+                        ms12.Close();
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("The response says that this file is not found!");
+            }
+        }
+        else if (temp == "3")
+        {
+            Console.WriteLine("Do you want to delete the file by name or by id (1 - name, 2 - id): >");
+            temp2 = Console.ReadLine();
+            if (temp2 == "1")
+            {
+                Console.WriteLine("Enter name: >");
+                temp2 = Console.ReadLine();
+                Console.WriteLine("The request was sent.");
+                client.Send(Encoding.UTF8.GetBytes("3`1`" + temp2));
+
+            }
+            else
+            {
+                Console.WriteLine("Enter ID: >");
+                temp2 = Console.ReadLine();
+                Console.WriteLine("The request was sent.");
+                client.Send(Encoding.UTF8.GetBytes("3`2`" + temp2 + "`"));
+            }
+            bytes = await client.ReceiveAsync(responseBytes);
+            response = Encoding.UTF8.GetString(responseBytes, 0, bytes);
+            var items = response.Split("`");
+            if (items[0] == "404")
+            {
+                Console.WriteLine("The response says that this file is not found!");
+            }
+            else
+            {
+                Console.WriteLine("The response says that this file was deleted successfully!");
+            }
         }
         else
         {
-            Console.WriteLine("Введите корректное значение! \n Enter action (1-put in a file,2 - get a file,3 delete a file,exit-for exit):>");
-            temp = Console.ReadLine();
+            Console.WriteLine("Enter name of the file: >");
+            temp2 = Console.ReadLine();
+            Console.WriteLine("Enter name of the file to be saved on server: >");
+            string temp3 = Console.ReadLine();
+            Console.WriteLine("The request was sent.");
+            saveFile(temp2, temp3, client, responseBytes, bytes, response);
         }
     }
-    Console.WriteLine("Enter filename:>");
-    zapros += Console.ReadLine() + "`";
-    if (temp == "1")
-    {
-        Console.WriteLine("Enter  file content: >");
-        zapros += Console.ReadLine();
-        Console.WriteLine("The request was sent.");
-        client.Send(Encoding.UTF8.GetBytes(zapros));
-        bytes = await client.ReceiveAsync(responseBytes);
-        response = Encoding.UTF8.GetString(responseBytes, 0, bytes);
-        if (response == "202")
-        {
-            Console.WriteLine("The response says that the file was created!");
-        }
-        else
-        {
-            Console.WriteLine("The response says that creating the file was forbidden!");
-        }
-    }
-    else if (temp == "2")
-    {
-        Console.WriteLine("The request was sent.");
-        client.Send(Encoding.UTF8.GetBytes(zapros));
-        bytes = await client.ReceiveAsync(responseBytes);
-        response = Encoding.UTF8.GetString(responseBytes, 0, bytes);
-        var temp1 = response.Split("`");
-        if (response[0].ToString() == "2")
-        {
-            Console.WriteLine("The content of the file is: " + temp1[1]);
-        }
-        else
-        {
-            Console.WriteLine("The response says that the file was not found!");
-        }
-    }
-    else
-    {
-        Console.WriteLine("The request was sent.");
-        client.Send(Encoding.UTF8.GetBytes(zapros));
-        bytes = await client.ReceiveAsync(responseBytes);
-        response = Encoding.UTF8.GetString(responseBytes, 0, bytes);
-        if (response == "200")
-        {
-            Console.WriteLine("The response says that the file was successfully deleted!");
-        }
-        else
-        {
-            Console.WriteLine("The response says that the file was not found!");
-        }
-    }
+
     client.Close();
 }
 catch
@@ -95,4 +150,61 @@ catch
 finally
 {
     client.Close();
+}
+
+
+string getOperation()
+{
+    string temp;
+    Console.WriteLine("Enter action (1-put in a file,2 - get a file,3 delete a file,exit-for exit):>");
+    temp = Console.ReadLine();
+    while (true)
+    {
+        if (temp == "1" || temp == "2" || temp == "3" || temp=="exit" )
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Введите корректное значение! \n Enter action (1-put in a file,2 - get a file,3 delete a file,exit-for exit):>");
+            temp = Console.ReadLine();
+        }
+    }
+    return temp;
+}
+
+
+async void saveFile(string fileName, string serverFile, Socket client, byte[] responseBytes, int bytes, string response)
+{
+    if (fileName.Contains("txt"))
+    {
+        var e = new FileInfo("client\\data\\" + fileName).Length;
+        var msg = "1`1`" + serverFile + "`" + e + "`";
+        var msg1 = File.ReadAllText("client\\data\\" + fileName);
+        await client.SendAsync(Encoding.UTF8.GetBytes(msg));
+        await client.SendAsync(Encoding.UTF8.GetBytes(msg1));
+    }
+    else
+    {
+        long e;
+        byte[] ms1;
+        using (var ms = new MemoryStream())
+        {
+            Image image = Image.FromFile("client\\data\\" + fileName);
+            image.Save(ms, image.RawFormat);
+            e = ms.Length;
+            ms1 = ms.ToArray();
+            ms.Close();
+        }
+        var msg = "1`2`" + serverFile + "`" + e + "`";
+        await client.SendAsync(Encoding.UTF8.GetBytes(msg));
+        await client.SendAsync(ms1);
+    }
+    bytes = client.Receive(responseBytes);
+    response = Encoding.UTF8.GetString(responseBytes, 0, bytes);
+    var items = response.Split("`");
+    if (items[0].Contains("202"))
+    {
+        Console.WriteLine("Response says that file is saved! ID =" + items[1]);
+    }
 }
--- Lab03Server/Lab03Server/Program.cs
+++ Lab03Server/Lab03Server/Program.cs
@@ -1,58 +1,26 @@
 ﻿using System.Net.Sockets;
 using System.Net;
 using System.Text;
+using System.Collections;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 8888);
 using Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 server.Bind(ipPoint);
 server.Listen();
 Console.WriteLine($"Server started!");
+Hashtable filesTable = new Hashtable();
+int voidId=1;
+putHashTable(ref filesTable,ref voidId);
 try
 {
     while (true) // Цикл для ожидания новых подключений
     {
-        using Socket client = await server.AcceptAsync();
-        Console.WriteLine($"Адрес подключенного клиента: {client.RemoteEndPoint}");
-        byte[] b = new byte[512];
-        var files = new List<(int, string)>();
-        try
-        {
-            string temp = "";
-            client.Receive(b);
-            string msg = Encoding.Default.GetString(b);
-            string[] zpr = new string[3];
-            zpr = msg.Split("`");
-            if (zpr[0].Contains("exit"))
-            {
-                client.Send(Encoding.UTF8.GetBytes("1"));
-                break;
-            }
-            else if (zpr[0] == "1")
-            {
-                var res = PUT(zpr[1], zpr[2]);
-                await client.SendAsync(Encoding.UTF8.GetBytes(res));
-            }
-            else if (zpr[0] == "2")
-            {
-                var res = GET(zpr[1]);
-                await client.SendAsync(Encoding.UTF8.GetBytes(res));
-            }
-            else
-            {
-                var res = DELETE(zpr[1]);
-                await client.SendAsync(Encoding.UTF8.GetBytes(res));
-            }
-            client.Close();
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Клиент отключился ");
-        }
-        finally
-        {
+        Socket client =await server.AcceptAsync();
+        Thread t = new Thread(async () => ProcessClientAsync(client));
+        t.Start();
 
-            client.Close();
-        }
     }
 }
 finally
@@ -61,22 +29,200 @@ finally
 }
 
 
-string PUT(string fileName,string text)
+async Task ProcessClientAsync(Socket client)
 {
-    if (File.Exists("Server\\data\\" + fileName))
+    Console.WriteLine($"Адрес подключенного клиента: {client.RemoteEndPoint}");
+    byte[] b = new byte[512];
+    var files = new List<(int, string)>();
+    try
     {
-        return "403";
-    }
-    else
-    {
-        using(var stream= File.Open("Server\\data\\" + fileName, FileMode.CreateNew,FileAccess.Write))
+        string temp = "";
+        client.Receive(b);
+        string msg = Encoding.Default.GetString(b);
+        var zpr = msg.Split("`");
+        if (zpr[0].Contains("exit"))
         {
-            stream.Write(Encoding.UTF8.GetBytes(text));
+            client.Close();
         }
-        return "202";
+        else if (zpr[0] == "1")
+        {
+            byte[] resp = new byte[Convert.ToInt32(zpr[3])]; client.Receive(resp);
+            if (zpr[1].Contains("txt"))
+            {
+                PUT(client, zpr[2], filesTable, resp);
+            }
+            else
+            {
+                PUT_image(client, zpr[2], filesTable, resp);
+            }
+            getHashTable(filesTable);
+        }
+        else if (zpr[0] == "2")
+        {
+            if (zpr[1] == "1")
+            {
+                var res = GET(zpr[2]);
+                await client.SendAsync(Encoding.UTF8.GetBytes(res));
+            }
+            else if (zpr[1]=="2")
+            {
+                var res = GETbyId(zpr[2], filesTable);
+                await client.SendAsync(Encoding.UTF8.GetBytes(res));
+            }
+            else if (zpr[1] == "3")
+            {
+                var res = GET_image(zpr[2]);
+                await client.SendAsync(Encoding.UTF8.GetBytes(res));
+                if (res.Contains("200"))
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        Image image = Image.FromFile("Server\\data\\" + zpr[2]);
+                        image.Save(ms, image.RawFormat);
+                        await client.SendAsync(ms.ToArray());
+                    }
+                }
+            }
+            else if (zpr[1] == "4")
+            {
+                var res = GET_imagebyId(zpr[2], filesTable);
+                await client.SendAsync(Encoding.UTF8.GetBytes(res));
+                if (res.Contains("200"))
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        Image image = Image.FromFile("Server\\data\\" + filesTable[zpr[2]]);
+                        image.Save(ms, image.RawFormat);
+                        await client.SendAsync(ms.ToArray());
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (zpr[1] == "1")
+            {
+                var res = DELETE(zpr[2], ref filesTable);
+                await client.SendAsync(Encoding.UTF8.GetBytes(res));
+                getHashTable(filesTable);
+            }
+            else
+            {
+                var res = DELETEbyID(zpr[2], ref filesTable);
+                await client.SendAsync(Encoding.UTF8.GetBytes(res));
+                getHashTable(filesTable);
+            }
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Клиент отключился ");
+    }
+    finally
+    {
+
+        client.Close();
     }
 }
 
+async void PUT(Socket client,string fileName,Hashtable a, byte[] responseBytes)
+{
+    if (fileName != "")
+    {
+        if (a.Contains(fileName))
+        {
+            client.SendAsync(Encoding.UTF8.GetBytes("403"));
+        }
+        else
+        {
+
+            client.SendAsync(Encoding.UTF8.GetBytes("202`" + voidId.ToString()));
+            using (var file = File.Open("Server\\data\\" + fileName, FileMode.CreateNew, FileAccess.Write))
+            {
+                file.Write(responseBytes);
+            }
+            a.Add( voidId.ToString(),fileName);
+            voidId=a.Count+1;
+        }
+    }
+    else
+    {
+
+        client.SendAsync(Encoding.UTF8.GetBytes("202`" + voidId.ToString()));
+        await using (var file = File.Open("Server\\data\\" + voidId + ".txt", FileMode.CreateNew, FileAccess.Write))
+        {
+            file.Write(responseBytes);
+        }
+        a.Add(voidId.ToString(),voidId + ".txt" );
+        voidId = a.Count + 1;
+    }
+}
+
+
+async void PUT_image(Socket client, string fileName, Hashtable a, byte[] responseBytes)
+{
+    if (fileName != "")
+    {
+        if (a.Contains(fileName))
+        {
+            client.SendAsync(Encoding.UTF8.GetBytes("403"));
+        }
+        else
+        {
+
+            client.SendAsync(Encoding.UTF8.GetBytes("202`" + voidId.ToString()));
+            using (var ms = new MemoryStream(responseBytes))
+            {
+                Image img=Image.FromStream(ms);
+                img.Save("Server\\data\\" + fileName,ImageFormat.Png);
+                ms.Close();
+            }
+            a.Add(voidId.ToString(), fileName);
+            voidId = a.Count + 1;
+        }
+    }
+    else
+    {
+
+        client.SendAsync(Encoding.UTF8.GetBytes("202`" + voidId.ToString()));
+        using (var ms = new MemoryStream(responseBytes))
+        {
+            Image img = Image.FromStream(ms);
+            img.Save("Server\\data\\" + voidId + ".png", ImageFormat.Png);
+            ms.Close();
+        }
+        a.Add(voidId.ToString(), voidId + ".png");
+        voidId = a.Count + 1;
+    }
+}
+
+
+
+void putHashTable(ref Hashtable a,ref int voidId)
+{
+    using(StreamReader reader =new StreamReader("indexes.txt"))
+    {
+        string? line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            a.Add(line.Split(" ")[0], line.Split(" ")[1]);
+            voidId++;
+        }
+    }
+}
+
+
+void getHashTable(Hashtable a)
+{
+    using(StreamWriter writer = new StreamWriter("indexes.txt",false))
+    {
+        ICollection keys = a.Keys.Cast<string>().OrderBy(c=>c).ToArray();
+        foreach (string s in keys)
+        {
+            writer.WriteLine(s+" " + a[s]);
+        }
+    }
+}
 string GET(string fileName)
 {
     if (!File.Exists("Server\\data\\" + fileName))
@@ -89,7 +235,63 @@ string GET(string fileName)
         return "200`"+temp;
     }
 }
-string DELETE(string fileName)
+
+string GET_image(string fileName)
+{
+    if (!File.Exists("Server\\data\\" + fileName))
+    {
+        return "404";
+    }
+    else
+    {
+        long e;
+        byte[] ms1;
+        using (var ms = new MemoryStream())
+        {
+            Image image = Image.FromFile("Server\\data\\" + fileName);
+            image.Save(ms, image.RawFormat);
+            e = ms.Length;
+            ms1 = ms.ToArray();
+            ms.Close();
+        }
+        return "200`"+ms1.Length+"`";
+    }
+}
+string GET_imagebyId(string fileID, Hashtable a)
+{
+    if (!a.ContainsKey(fileID))
+    {
+        return "404";
+    }
+    else
+    {
+        long e;
+        byte[] ms1;
+        using (var ms = new MemoryStream())
+        {
+            Image image = Image.FromFile("Server\\data\\" + a[fileID]);
+            image.Save(ms, image.RawFormat);
+            e = ms.Length;
+            ms1 = ms.ToArray();
+            ms.Close();
+        }
+        return "200`" + ms1.Length + "`";
+    }
+}
+
+string GETbyId(string fileID,Hashtable a)
+{
+    if (!a.ContainsKey(fileID))
+    {
+        return "404";
+    }
+    else
+    {
+        string temp = File.ReadAllText("Server\\data\\" + a[fileID]);
+        return "200`" + temp;
+    }
+}
+string DELETE(string fileName, ref Hashtable a)
 {
     if (!File.Exists("Server\\data\\" + fileName))
     {
@@ -98,6 +300,30 @@ string DELETE(string fileName)
     else
     {
         File.Delete("Server\\data\\" + fileName);
+        foreach (var key in a.Keys)
+        {
+            if (a[key]==fileName)
+            {
+                a.Remove(key);
+                voidId = Convert.ToInt16(key);
+                break;
+            }
+        }
+        return "200";
+    }
+}
+
+string DELETEbyID(string fileID,ref Hashtable a)
+{
+    if (!a.ContainsKey(fileID))
+    {
+        return "404";
+    }
+    else
+    {
+        File.Delete("Server\\data\\" + a[fileID]);
+        a.Remove(fileID);
+        voidId = Convert.ToInt16(fileID);
         return "200";
     }
 }
